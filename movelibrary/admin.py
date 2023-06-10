74cc038b532cf503894ecacd5083db145620bace
Cal-Rex <andy.gibb@healthcareskills.com>
2023-06-10 13:51:39 +0000

    add search function to admin panel

--- movelibrary/admin.py
+++ movelibrary/admin.py
@@ -7,6 +7,8 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     prepopulated_fields = {'slug': ('movement_name',)}
+    list_display = ('movement_name', 'vid_link')
+    search_fields = ['movement_name']
 
 
 admin.site.register(Tag)
