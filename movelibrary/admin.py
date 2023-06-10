f822dd3d13b84424b22a0e1534250ca647b5e1f5
Cal-Rex <andy.gibb@healthcareskills.com>
2023-06-10 13:16:34 +0000

    admin panel: add slug auto-fill feature, make movement objects list by name on admin panel

--- movelibrary/admin.py
+++ movelibrary/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Movement, Tag
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Movement)
+class PostAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('movement_name',)}
+
+
+admin.site.register(Tag)
+
+# Alternative method for if multiple arguments or packages
+# needed to be added to tables:
+# @admin.register(MovementLibrary)
+# @admin.register(Tags)
