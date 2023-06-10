1f0001dfe5d05de17e970adfe9db35d6da4d6a52
Cal-Rex <andy.gibb@healthcareskills.com>
2023-06-10 15:29:07 +0000

    connect up models to views, and views to templates

--- movelibrary/views.py
+++ movelibrary/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Movement, Tag, UserNonAuthField
 
 # Create your views here.
+
+
+class LastMovement(generic.ListView):
+    model = Movement
+    template_name = 'index.html'
+    paginate_by = 1
