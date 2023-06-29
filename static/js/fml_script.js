4969123365a588f61f3846c2dd1ff08f5d8401e5
Cal-Rex <andy.gibb@healthcareskills.com>
2023-06-29 14:47:42 +0000

    add jquery cdn

--- static/js/fml_script.js
+++ static/js/fml_script.js
@@ -1,23 +1,15 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// })
+    
+    $('.record-edit').click(function(){
+        let recordId = $("#" + fieldset_id);
+        let movementSlug = $('#slug');
+        console.log(`recordId: ${recordId}`);
+        console.log(`movementSlug: ${movementSlug}`);
+        $('#edit-record-form').attr('action', `/edit_one_rm/${movementSlug}/${recordId}/`);
+
+    });
 
 
-// code given from user on google's issue tracker
-// tackles video load failure on first load attempt
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+});
 
-const popover = new bootstrap.Popover('.btn', {
-    container: '.offcanvas-body'
-  })
-
-let player = document.getElementById('mvmnt') 
-
-function onStateChange(e) {
-    if (e.data === YT.PlayerState.ENDED) {
-        player.playVideo();
-    }
-}
-
-player.addEventListener(onload, onStateChange)
