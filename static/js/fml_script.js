c2f3b05e5ac8f73cf892248dbc5ffee3ab0e6b00
Cal-Rex <andy.gibb@healthcareskills.com>
2023-06-30 14:32:34 +0000

    add js to make edit 1RM form work

--- static/js/fml_script.js
+++ static/js/fml_script.js
@@ -1,15 +1,16 @@
-$(document).ready(function() {
+// _____________________________________________________________
+// as of jquery 3.0, .ready event handler has been deprecated, 
+// recommended alternative is now to wrap all code in a function
+// https://api.jquery.com/ready/
+// _____________________________________________________________
+$(function (e) {
 
-    
-    $('.record-edit').click(function(){
-        let recordId = $("#" + fieldset_id);
-        let movementSlug = $('#slug');
-        console.log(`recordId: ${recordId}`);
-        console.log(`movementSlug: ${movementSlug}`);
+    // 1 Rep Max Related functions
+    // Function to apply relevant post "action" to Edit 1RM form
+    $('.record-edit').on('click', function (e) {
+        let recordId = $(this).attr("id");
+        let movementSlug = document.getElementById('slug').innerHTML;
         $('#edit-record-form').attr('action', `/edit_one_rm/${movementSlug}/${recordId}/`);
-
     });
-
-
+      
 });
-
