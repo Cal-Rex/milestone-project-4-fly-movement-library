189bf3f8fbacc2f402465a7c18c0a8ed5f2fdcf8
Cal-Rex <andy.gibb@healthcareskills.com>
2023-07-08 17:29:27 +0000

    trying media storage location shange again

--- FlyMovementLibrary/settings.py
+++ FlyMovementLibrary/settings.py
@@ -150,7 +150,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/'
+MEDIA_URL = ''
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
