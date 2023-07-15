c2a1173b9fff1dfd8a3187d6954258949ea5999a
Cal-Rex <andy.gibb@healthcareskills.com>
2023-07-15 01:12:36 +0000

    build responsive search function in javascript

---
+++ static/js/base-search.js
@@ -0,0 +1,27 @@
+$(function () {
+
+    $(`.query`).css('display', 'none');
+
+    $('#library-search').on('keyup', function () {
+        $(`.query`).css('display', 'none');
+        var search = $('#library-search').val();
+    
+        let querySet = document.getElementsByClassName('query')
+    
+        for (value of querySet) {
+            criteriaMatch = false;
+            query = value.id;
+            query = query.split("-");
+            for (word of query) {
+                if (word.includes(search)) {
+                    criteriaMatch = true
+                }
+            if (criteriaMatch == true) {
+                document.getElementById(value.id).style.display = 'inline';
+            }
+            }
+        }
+
+    });
+
+});
