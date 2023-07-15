fcb51da97cd9300645d9359ca00325115bbc3e40
Cal-Rex <andy.gibb@healthcareskills.com>
2023-07-15 22:39:20 +0000

    remove custom icon list class in movement

--- static/js/movement.js
+++ static/js/movement.js
@@ -2,6 +2,5 @@ $(function () {
     $('#movement-directions').children('div').eq(0).addClass('directions-heading');
     $('#movement-directions').children('div').eq(1).addClass('directions-heading');
 
-    $('#movement-directions').children('ul').addClass('custom-icon-list');
     $('#movement-directions').children('ul').children('li').addClass('directions-list-items');
 });
