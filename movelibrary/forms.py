a920245c8265170d13293e30cf632c127c31523a
Cal-Rex <andy.gibb@healthcareskills.com>
2023-07-17 06:24:33 +0000

    cleanup admin.py

--- movelibrary/forms.py
+++ movelibrary/forms.py
@@ -1,18 +1,12 @@
-# imports the Item class from the local models.py file
 from .models import UserOneRepMax, User, UserNonAuthField
-# imports the forms library from django
 from django import forms
 from allauth.account.forms import SignupForm
 
 
-# class calls on the Modelform class inside the forms library from django
 class OneRmForm(forms.ModelForm):
-    # class meta used here to establish the metadata of the form element
+
     class Meta:
-        # the model is the imported Item model from models.py
         model = UserOneRepMax
-        # then the fields are listed as a python list of strings
-        # that match the variable names
         fields = ('one_rep_max',)
 
 
